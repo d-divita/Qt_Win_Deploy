53484defbe29b66708efb95ab2b14c7d56e077a6
d-divita <50867320+d-divita@users.noreply.github.com>
2019-12-12 10:58:28 +0100

    Update quick_deploy.bat

--- quick_deploy.bat
+++ quick_deploy.bat
@@ -1,3 +1,3 @@
 set PATH=%PATH%;C:\Qt\5.13.0\mingw73_32\bin
 set PATH=%PATH%;C:\Qt\Tools\mingw730_32\bin
-windeployqt.exe --no-translations --force C:\Users\ddivi\OneDrive\Desktop\Deploy\GAMMA_GUI.exe
+windeployqt.exe --no-translations --force app_name.exe

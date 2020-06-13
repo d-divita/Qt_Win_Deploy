1d47608939c0386d52c9885a0eed6279ca0673b9
d-divita <50867320+d-divita@users.noreply.github.com>
2020-06-13 16:26:07 +0200

    Updated to Qt 5.15.0 with mingw 8.1

--- quick_deploy64.bat
+++ quick_deploy64.bat
@@ -1,3 +1,3 @@
-set PATH=%PATH%;C:\Qt\5.13.0\mingw73_64\bin
-set PATH=%PATH%;C:\Qt\Tools\mingw730_64\bin
+set PATH=%PATH%;C:\Qt\5.15.0\mingw81_64\bin
+set PATH=%PATH%;C:\Qt\Tools\mingw810_64\bin
 windeployqt.exe --no-translations --force app_name.exe

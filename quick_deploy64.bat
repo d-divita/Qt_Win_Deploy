a9349d53e501e095eaca393edfe3ab3c00699a05
d-divita <50867320+d-divita@users.noreply.github.com>
2023-05-19 12:01:32 +0200

    Update quick_deploy64.bat
    added a pause at the end and updated the default path for Qt 5.15.2

--- quick_deploy64.bat
+++ quick_deploy64.bat
@@ -1,3 +1,4 @@
-set PATH=%PATH%;C:\Qt\5.15.0\mingw81_64\bin
+set PATH=%PATH%;C:\Qt\5.15.2\mingw81_64\bin
 set PATH=%PATH%;C:\Qt\Tools\mingw810_64\bin
 windeployqt.exe --no-translations --force app_name.exe
+PAUSE
